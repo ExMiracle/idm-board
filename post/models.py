0bd071572f981acdf84b1b838c15adef01d7c3ac
ExMiracle <exmiracle2@gmail.com>
2019-09-20 01:03:14 +0300

    Initial commit

---
+++ post/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(blank=True, null=True, upload_to='blog_pics')
+
+
+    def __str__(self):
+        return (self.title)
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
